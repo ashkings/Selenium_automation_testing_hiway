bb7abe3fe7dcc861a3ebe8b072388174759abee6
ashu <ashu.singla@hashedin.com>
2019-08-20 23:33:34 +0530

    Opening of highway website in web browser

--- pages/open_hiway_url.py
+++ pages/open_hiway_url.py
@@ -1,11 +1,9 @@
-from selenium import webdriver
+class OpenHighway:
+    def __init__(self, driver):
+        self.driver = driver
+        self.baseUrl = 'https://qa.hiway.hashedin.com'
 
-
-class OpenUrl:
-    baseUrl = 'https://qa.hiway.hashedin.com'
-
-    def __init__(self, path="/home/ashu/Desktop/Selenium_Tracks/web_drivers/chromedriver"):
-        self.browser = webdriver.Chrome(path)
-        self.browser.implicitly_wait(20)
-        self.browser.maximize_window()
-        self.browser.get(self.baseUrl)
+    def get_url(self):
+        self.driver.implicitly_wait(20)
+        self.driver.maximize_window()
+        self.driver.get(self.baseUrl)
