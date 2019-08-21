0ad8092fcadb88cdfe555e94d01bc6903a9a8cbf
ashu <ashu.singla@hashedin.com>
2019-08-21 14:39:29 +0530

    Testcases for accessible username and password for highway

---
+++ constant.py
@@ -0,0 +1,2 @@
+chrome_driver_path = '/home/ashu/Desktop/Selenium_Tracks/web_drivers/chromedriver'
+firefox_driver_path = '/home/ashu/Desktop/Selenium_Tracks/web_drivers/geckodriver'
--- test/test_login.py
+++ test/test_login.py
@@ -1,13 +1,11 @@
-import pytest
+import pytest, unittest, constant
 
+from Utility.csv_loader import get_csv_data
 from pages.open_hiway_url import OpenHighway
 from pages.login_google import LoginUsingGoogle
 from pages.driver import Driver
 from pages.enter_login_credentials import LoginCredentials
-from pages.start_using_application import StartApplication
-from pages.timesheet import Timesheet
-from ddt import ddt, file_data, unpack
-import csv
+from ddt import ddt, data, unpack
 
 
 @pytest.fixture
@@ -15,18 +13,28 @@ def browser_name(request):
     return request.config.getoption("--browser")
 
 
-@pytest.yield_fixture()
-def setup(browser_name):
-    driver = Driver(browser_name)
-    global browser
-    browser = driver.get_path()
-    highway = OpenHighway(browser)
-    highway.get_url()
-    login_page = LoginUsingGoogle(browser)
-    login_page.click_on_login()
+@ddt
+class TestLogin(unittest.TestCase):
+    @pytest.yield_fixture()
+    def setup(self, browser_name):
+        driver = Driver(browser_name)
+        global browser
+        browser = driver.get_path()
+        highway = OpenHighway(browser)
+        highway.get_url()
+        login_page = LoginUsingGoogle(browser)
+        login_page.click_on_login()
+        yield
+        browser.close()
 
-@file_data('/home/ashu/Desktop/Selenium_Tracks/login.csv')
-def test_login(username, password):
-    login_values = LoginCredentials(browser)
-    login_values.enter_username(username)
-    login_values.enter_password(password)
+    @pytest.mark.usefixtures("setup")
+    @data(*get_csv_data(constant.csv_path_for_successful_login))
+    @unpack
+    def test_successful_login(self, username, password):
+        login_values = LoginCredentials(browser)
+        login_values.enter_username(username)
+        login_values.enter_password(password)
+
+
+if __name__ == '__main__':
+    pytest.main()
